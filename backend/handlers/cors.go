2e1b50faea9c673e4d5649a315ce5298d83a22d4
mjohanbintangp <m.johanbintang.pratama@gmail.com>
2025-06-20 15:37:20 +0700

    update CORS

--- backend/handlers/cors.go
+++ backend/handlers/cors.go
@@ -3,7 +3,7 @@ package handlers
 import "net/http"
 
 func enableCORS(w http.ResponseWriter) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Origin", "https://flexnative.vercel.app")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 }
